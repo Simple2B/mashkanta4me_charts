ee644f4fd0d83732e56b2076c4fbd3d1c077bca4
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-04-05 13:09:22 +0300

    style arror message

--- app/static/js/auth_modal.js
+++ app/static/js/auth_modal.js
@@ -2,79 +2,106 @@ let wpAuthModal;
 
 document.addEventListener("DOMContentLoaded", function(event) {
     wpAuthModal = document.getElementById('authModal');
-
     const openButton = document.getElementById('openModal');
+    const closeButton = document.getElementById('closeModal');
+    const openLogin = document.getElementById('pro-content-login');
+    const authorizationModal = document.getElementById('authorizationModal');
+    const closeModalPopup = document.getElementById('closeModalPopup');
+    const mailError = document.getElementById('err-email');
+    const passErr = document.getElementById('err-pass');
+
     openButton.addEventListener('click', (evt) => {
         wpAuthModal.style.display = 'block';
     });
 
-    const closeButton = document.getElementById('closeModal');
     closeButton.addEventListener('click', (evt) => {
         wpAuthModal.style.display = 'none';
     });
 
-    const openLogin = document.getElementById('pro-content-login');
-    const authorizationModal = document.getElementById('authorizationModal');
     openLogin.addEventListener('click', (evt) => {
         wpAuthModal.style.display = 'none';
         authorizationModal.style.display = 'block';
     });
 
-    const closeModalPopup = document.getElementById('closeModalPopup');
     closeModalPopup.addEventListener('click', (evt) => {
         authorizationModal.style.display = 'none';
     });
 
+    //test 
+    // const btnSubmit = document.getElementById('show-register');
 
-    authorizationModal.addEventListener('submit', (evt) => {
-      evt.preventDefault();
-      const email = formatData(document.getElementById('login-user-email').value);
-      const pass = document.getElementById('login-user-pass').value.trim();
-      const remember = document.getElementById('login-user-remember').checked;
-      const data = {
-          action: 'flask_auth',
-          email: email,
-          pass: pass,
-          remember: remember,
-      };
-console.log(data);
-      $.ajaxSetup({
-          crossDomain: true,
-          xhrFields: {
-              withCredentials: true
-          },
-      });
+    // btnSubmit.addEventListener('click', (e) => {
+    //         e.preventDefault();
+    //         const email = formatData(document.getElementById('login-user-email').value);
+    //         const pass = document.getElementById('login-user-pass').value.trim();
+    //         const remember = document.getElementById('login-user-remember').checked;
 
-      const ajax_url = document.location.origin.concat('/wp-admin/admin-ajax.php');
-      const mailError = document.getElementById('err-email');
-      const passErr = document.getElementById('err-pass');
+    //         const data = {
+    //             action: 'flask_auth',
+    //             email: email,
+    //             pass: pass,
+    //             remember: remember,
+    //         };
+    //         mailError.style.visibility = "visible";
 
-      $.post(ajax_url, data, (res) => {
-          console.log(res);
-          if (res.success) {
-              document.location.reload();
-              return;
-          }
-          if (res.data.error === "email") {
+    //         mailError.innerHTML = "emailerror emailerror email emailerror emailerror email emailerror emailerror emailemailerror emailerror email";
 
-              mailError.innerHTML = res.data.message;
-              passErr.innerHTML = " ";
+    //     })
+    //end of test
 
-          } else {
+    authorizationModal.addEventListener('sumbit', (evt) => {
+        evt.preventDefault();
+        const email = formatData(document.getElementById('login-user-email').value);
+        const pass = document.getElementById('login-user-pass').value.trim();
+        const remember = document.getElementById('login-user-remember').checked;
 
-              passErr.innerHTML = res.data.message;
-              mailError.innerHTML = " ";
-          }
-      })
+        const data = {
+            action: 'flask_auth',
+            email: email,
+            pass: pass,
+            remember: remember,
+        };
+        alert(data);
+        console.log(data);
+        $.ajaxSetup({
+            crossDomain: true,
+            xhrFields: {
+                withCredentials: true
+            },
+        });
 
-      return false;
-  });
+        const ajax_url = document.location.origin.concat('/wp-admin/admin-ajax.php');
+        const mailError = document.getElementById('err-email');
+        const passErr = document.getElementById('err-pass');
 
-  function formatData(strData) {
-    return strData.toLowerCase().trim();
-}
+        $.post(ajax_url, data, (res) => {
+            console.log(res);
+            if (res.success) {
+                document.location.reload();
+                return;
+            }
+            if (res.data.error === "email") {
+                mailError.style.visibility = "visible";
+                mailError.innerHTML = res.data.message;
+                passErr.style.visibility = "none";
+                passErr.innerHTML = " ";
+
+            } else {
+                passErr.style.visibility = "visible";
+                passErr.innerHTML = res.data.message;
+                mailError.style.visibility = "none";
+                mailError.innerHTML = " ";
+            }
+        })
+
+        return false;
+    });
+
+    function formatData(strData) {
+        return strData.toLowerCase().trim();
+    }
+
 });
-
 
 const filterButtons = document.querySelectorAll('.filter-button');
 for (let i = 0, n = filterButtons.length; i < n; i++) {
