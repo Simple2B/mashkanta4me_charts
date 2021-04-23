7dd9ba91ea6568ce53eb0770bf639539ce7ac235
Nikolay <nchernov@gmail.com>
2021-04-23 20:10:01 +0300

    fix make depoy zip

--- make_deploy_zip.sh
+++ make_deploy_zip.sh
@@ -1,4 +1,4 @@
 ZIP_FILE_NAME="mashkanta_charts.zip"
 echo $ZIP_FILE_NAME
 rm $ZIP_FILE_NAME
-zip -r $ZIP_FILE_NAME  app/ data/ wp-content/ .env config.py requirements.txt tox.ini wsgi.py
+zip -r $ZIP_FILE_NAME  app/ data/ wp-content/ config.py requirements.txt tox.ini wsgi.py
